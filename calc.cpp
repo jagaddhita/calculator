05a1fcfd72a470a9e9e01be38f2ab28ad2e01d4a
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-08-18 21:19:25 -0700

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // menampilkan pertambahan//
+    
+    int bilangan1;
+    int bilangan2;
+    int hasil;
+    hasil = bilangan1+bilangan2;
+    
+    cout << "Bilangan pertama = " ;
+    cin >> bilangan1; cout; endl;
+    cout << "Bilangan kedua = " ;
+    cin >> bilangan2; cout; endl;
+    cout << "Hasil pertambahan = " <<hasil; cout<< endl;
+    
+    return 0;    
+}
